488b3058fd135bd8286c7b1a48b632cfebe1e771
Damian Corvalan <damiancorvalan1987@gmail.com>
2021-05-02 22:00:13 -0300

    metprod

--- Datos/DProducto.cs
+++ Datos/DProducto.cs
@@ -9,5 +9,46 @@ namespace Datos
     class DProducto
     {
         //crear obj producto hardcodeado y retornar
+
+        public void Insertar(Producto item) {                      
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsetarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre",item.nombre);
+            comando.Parameters.AddWithValue("@categoria",item.categ);
+            comando.Parameters.AddWithValue("@idproducto",item.idprod);
+            comando.Parameters.AddWithValue("@preciocompra",item.preciocompra);
+            comando.Parameters.AddWithValue("@precioventa",item.precioventa);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        
+        }
+
+        public void Editar(Productos item) {                      
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre",item.nombre);
+            comando.Parameters.AddWithValue("@categoria",item.categ);
+            comando.Parameters.AddWithValue("@idproducto",item.idprod);
+            comando.Parameters.AddWithValue("@preciocompra",item.preciocompra);
+            comando.Parameters.AddWithValue("@precioventa",item.precioventa);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        
+        }
+
+        public void Eliminar(int idProducto) {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarProducto";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idProducto",idprod);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
     }
 }
